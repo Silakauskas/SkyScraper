cfdae63157057183205662a6e2ebce4da3d79a0f
DESKTOP-706MNH8\SS <simux9@gmail.com>
2022-02-05 13:33:04 +0200

    Changed user-agent to bypass hidenseek. Moved sports/tournament/championship to variable. Dealing with Lithuanian letters.

--- 0_scraper.py
+++ 0_scraper.py
@@ -1,5 +1,5 @@
-# Comment
 # import libraries
+# coding: utf-8
 import urllib3
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,11 +16,15 @@ from time import gmtime, strftime
 import os
 import json
 import csv
-
 from bs4 import BeautifulSoup
 
-DATA_PATH = "raw\\NBA\\"
-RESULTS_PATH = "logs\\NBA\\"
+# Import unidecode module from unidecode
+from unidecode import unidecode
+
+sports = "ICE_HOCKEY"
+
+DATA_PATH = "raw\\"+sports+"\\"
+RESULTS_PATH = "logs\\"+sports+"\\"
 TRACK_ENDED = True
 odds = {}
 finished = {}
@@ -34,25 +38,29 @@ def getEventFileName(event):
 def createNewEventLogFile(event):
     with open(RESULTS_PATH+getEventFileName(event['id']), 'w') as outcsv: # create this file
         writer = csv.writer(outcsv)
-        writer.writerow(["Date", event["teams"]["home"], "X", event["teams"]["away"], event['id']])
+        writer.writerow(["Date", unidecode(event["teams"]["home"]), "X", unidecode(event["teams"]["away"]), event['id']])
         outcsv.close()
 
 headers = {
-         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
+         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.81 Safari/537.36',
          'Content-Type': 'application/json'
 }
 
 #ORAKULAS - NBA
-response = requests.get(url="https://nodejs08.tglab.io/cache/3/lt/lt/394/prematch-by-championship.json",
+#response = requests.get(url="https://nodejs08.tglab.io/cache/3/lt/lt/394/prematch-by-championship.json",
+#response = requests.get(url="https://nodejs08.tglab.io/cache/5/lt/lt/Europe-Vilnius/events-by-path.json?path=futbolas%7Cpasaulis%7Cpasaulio-cempionatas-2022-kvalifikacija-europa&hidenseek=9a78299aed2ce1444f657aab7fe9945bde4047b3",
+response = requests.get(url="https://nodejs08.tglab.io/cache/3/lt/lt/14348/prematch-by-tournaments.json?hidenseek=82d1fedcdfd03ccf9504260c58e2e7b1830b029e",
                         headers=headers)
 
 json_object = response.json()
+
 
 ############ SAVE RAW FILE
 # TODO:  Check if tournament is not null
 raw_filename = 'cbet_' + strftime("%m_%d_%H_%M", gmtime()) + '.json'
 
-with open('raw/'+json_object['events'][0]['tournament_name']+'/' + raw_filename, 'w') as outfile:
+#with open('raw/'+json_object['events'][0]['tournament_name']+'/' + raw_filename, 'w') as outfile:
+with open("raw/"+sports+"/" + raw_filename, 'w') as outfile:
     json.dump(json_object, outfile, indent=4)
    
 for event in json_object['events']:
