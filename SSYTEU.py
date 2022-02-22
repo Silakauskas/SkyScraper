86c2a01d4a9e4db8221241e917daa85624a6ba7e
DESKTOP-706MNH8\SS <simux9@gmail.com>
2022-02-22 17:25:21 +0200

    Saving YouTube scrape results into a .csv file

--- SSYTEU.py
+++ SSYTEU.py
@@ -4,7 +4,23 @@
 import googleapiclient.discovery
 import requests
 import urllib3
+import os
 import json
+import csv
+from time import gmtime, strftime
+
+sports = "EUROVISION_YT"
+RESULTS_PATH = "logs\\"+sports+"\\"
+
+if not os.path.exists(RESULTS_PATH):
+    os.makedirs(RESULTS_PATH)
+
+if ("EUROVISION_YT.csv" not in os.listdir(RESULTS_PATH)):
+    with open(RESULTS_PATH+"EUROVISION_YT.csv", 'w') as outcsv: # create this file
+        writer = csv.writer(outcsv)
+        outcsv.close()
+
+
 # API information
 api_service_name = "youtube"
 api_version = "v3"
@@ -13,18 +29,8 @@ DEVELOPER_KEY = "AIzaSyBlELBZq_ZCHo6-C1cCDklA1V5dal7qQPo"
 # API client
 youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
-# Request body
-request = youtube.search().list(
-        part="id,snippet",
-        type='video',
-        q="Spider-Man",
-        videoDuration='short',
-        videoDefinition='high',
-        maxResults=1,
-        fields="items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))"
-)
 
-resp = requests.get(url="https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCRpjHHu8ivVWs73uxHlWwFA&maxResults=20&key=AIzaSyBlELBZq_ZCHo6-C1cCDklA1V5dal7qQPo")
+resp = requests.get(url="https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCRpjHHu8ivVWs73uxHlWwFA&maxResults=40&key=AIzaSyBlELBZq_ZCHo6-C1cCDklA1V5dal7qQPo")
 
 r = resp.text.encode('utf8').decode('ascii', 'ignore')
 print(resp.status_code)
@@ -32,14 +38,21 @@ print(resp.url)
 #print(r)
 json_object = json.loads(r)
 for video in json_object["items"]:
-    rv = youtube.videos().list(
-    part="statistics,contentDetails",
-        id=video["id"]["videoId"],
-        fields="items(statistics,contentDetails(duration))"
-    ).execute()
-    print(video["id"]["videoId"])
-    print(video["snippet"]["title"])
-    print(rv['items'][0]['statistics']['viewCount'])
-    print(rv['items'][0]['statistics']['likeCount'])
-    print(rv['items'][0]['statistics']['commentCount'])
-    print(" ")
+    csv_row = [strftime("%m_%d_%H_%M", gmtime())]
+    if "2022" in video["snippet"]["title"]:
+        rv = youtube.videos().list(
+            part="statistics,contentDetails",
+            id=video["id"]["videoId"],
+            fields="items(statistics,contentDetails(duration))"
+        ).execute()
+        csv_row.append(video["id"]["videoId"])
+        csv_row.append(video["snippet"]["title"])
+        csv_row.append(video["snippet"]["publishedAt"])
+        csv_row.append(rv['items'][0]['statistics']['viewCount'])
+        csv_row.append(rv['items'][0]['statistics']['likeCount'])
+        csv_row.append(rv['items'][0]['statistics']['commentCount'])
+
+        with open(RESULTS_PATH+"EUROVISION_YT.csv", 'a', newline='') as outcsv: # create this file
+            writer = csv.writer(outcsv)
+            writer.writerow(csv_row)
+            outcsv.close()
