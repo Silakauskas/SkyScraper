5b62bd62ae3fd3d47a059300d149827f0d5710b8
UbuntuSS <simonas.silakauskas@gmail.com>
2022-04-11 13:00:22 +0000

    Updating other files with configuration from linux

--- 1_scraper.py
+++ 1_scraper.py
@@ -21,16 +21,16 @@ from bs4 import BeautifulSoup
 # Import unidecode module from unidecode
 from unidecode import unidecode
 
-sports = "BAFTA"
+sports = "OSKARAI"
 
-DATA_PATH = "raw\\"+sports+"\\"
-RESULTS_PATH = "logs\\"+sports+"\\"
+DATA_PATH = "raw/"+sports+"/"
+RESULTS_PATH = "logs/"+sports+"/"
 TRACK_ENDED = True
 odds = {}
 finished = {}
 
 def doesEventLogFileExist(event):
-    return (getEventFileName(event) in os.listdir(RESULTS_PATH))
+    return (getEventFileName(event) in os.listdir())
 
 def getEventFileName(event):
     return str(event)+".csv"
@@ -42,7 +42,7 @@ def createNewEventLogFile(event):
         outcsv.close()
         
 def createNewEventLogFile(name):
-    with open(RESULTS_PATH+getEventFileName(name), 'w') as outcsv: # create this file
+    with open(getEventFileName(name), 'w') as outcsv: # create this file
         writer = csv.writer(outcsv)
         outcsv.close()
         
@@ -52,37 +52,39 @@ if not os.path.exists(DATA_PATH):
     os.makedirs(RESULTS_PATH)
 
 headers = {
-         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
-         'Content-Type': 'application/json'
+         #'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
+        'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+        'Content-Type': 'application/json'
 }
 
 #CBET - BAFTA
-response = requests.get(url="https://nodejs08.tglab.io/cache/3/lt/lt/20228/prematch-by-tournaments.json?hidenseek=69815cb5ce8fb6f651be2e741a91a8c48695b945",
-                        headers=headers)
+#response = requests.get(url="https://nodejs08.tglab.io/cache/3/lt/lt/20228/prematch-by-tournaments.json?hidenseek=69815cb5ce8fb6f651be2e741a91a8c48695b945",
+#                        headers=headers)
+response = requests.get(url="https://nodejs08.tglab.io/cache/3/lt/lt/19817/prematch-by-tournaments.json?hidenseek=71d96d9ac3c4099896b35afa4070aa096847f0d4", headers=headers)
 
 json_object = response.json()
-
+print(response)
 ############ SAVE RAW FILE
 # TODO:  Check if tournament is not null
 raw_filename = 'cbet_' + strftime("%m_%d_%H_%M", gmtime()) + '.json'
 
 print("Scraping "+sports+"...")
 
-with open("raw/"+sports+"/" + raw_filename, 'w') as outfile:
-    json.dump(json_object, outfile, indent=4)
+#with open("raw/"+sports+"/" + raw_filename, 'w') as outfile:
+#    json.dump(json_object, outfile, indent=4)
    
 for event in json_object['events'][0]['main_odds']['outright']['custom'].values():
     if not doesEventLogFileExist(sports):
         print("Log file for event",sports,"does not exist. Creating...")
         createNewEventLogFile(sports)
-        
+
     csv_row = [strftime("%m_%d_%H_%M", gmtime())]
     csv_row.append(unidecode(event['name']))
     csv_row.append(unidecode(event['team_name']))
     csv_row.append(unidecode(event['odd_expr']))
     csv_row.append(event['odd_value'])
-    
-    with open(RESULTS_PATH + getEventFileName(sports), 'a', newline='') as outcsv:  # create this file
+
+    with open(getEventFileName(sports), 'a', newline='') as outcsv:  # create this file
         writer = csv.writer(outcsv)
         writer.writerow(csv_row)
 
--- SSYTEU.py
+++ SSYTEU.py
@@ -5,21 +5,26 @@ import googleapiclient.discovery
 import requests
 import urllib3
 import os
+import os.path
+from os import path
 import json
 import csv
 from time import gmtime, strftime
 
 sports = "EUROVISION_YT"
-RESULTS_PATH = "logs\\"+sports+"\\"
+RESULTS_PATH = "logs"
 
 if not os.path.exists(RESULTS_PATH):
     os.makedirs(RESULTS_PATH)
 
-if ("EUROVISION_YT.csv" not in os.listdir(RESULTS_PATH)):
-    with open(RESULTS_PATH+"EUROVISION_YT.csv", 'w') as outcsv: # create this file
+if (not path.exists("EUROVISION_YT.csv")):
+    with open("EUROVISION_YT.csv", 'w') as outcsv: # create this file
         writer = csv.writer(outcsv)
         outcsv.close()
 
+fileVidIds = open('/home/ubuntu/vidIds.txt', 'r')
+lines = fileVidIds.read().splitlines()
+#print(lines)
 
 # API information
 api_service_name = "youtube"
@@ -30,21 +35,28 @@ DEVELOPER_KEY = "AIzaSyBlELBZq_ZCHo6-C1cCDklA1V5dal7qQPo"
 youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-resp = requests.get(url="https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCRpjHHu8ivVWs73uxHlWwFA&maxResults=40&key=AIzaSyBlELBZq_ZCHo6-C1cCDklA1V5dal7qQPo")
+resp = requests.get(url="https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=UCRpjHHu8ivVWs73uxHlWwFA&maxResults=50&key=AIzaSyBlELBZq_ZCHo6-C1cCDklA1V5dal7qQPo")
 
 r = resp.text.encode('utf8').decode('ascii', 'ignore')
 print(resp.status_code)
 print(resp.url)
+print(strftime("%m_%d_%H_%M", gmtime()))
+print(" ")
+
 #print(r)
 json_object = json.loads(r)
 for video in json_object["items"]:
     csv_row = [strftime("%m_%d_%H_%M", gmtime())]
-    if "2022" in video["snippet"]["title"]:
+    if "2022" in video["snippet"]["title"] and "videoId" in video["id"].keys():
         rv = youtube.videos().list(
             part="statistics,contentDetails",
             id=video["id"]["videoId"],
             fields="items(statistics,contentDetails(duration))"
         ).execute()
+        
+        #videoinfo = rv.text.encode('utf8').decode('ascii','ignore')
+        #print(rv)
+        
         csv_row.append(video["id"]["videoId"])
         csv_row.append(video["snippet"]["title"])
         csv_row.append(video["snippet"]["publishedAt"])
@@ -52,7 +64,7 @@ for video in json_object["items"]:
         csv_row.append(rv['items'][0]['statistics']['likeCount'])
         csv_row.append(rv['items'][0]['statistics']['commentCount'])
 
-        with open(RESULTS_PATH+"EUROVISION_YT.csv", 'a', newline='') as outcsv: # create this file
+        with open("EUROVISION_YT.csv", 'a', newline='') as outcsv: # create this file
             writer = csv.writer(outcsv)
             writer.writerow(csv_row)
             outcsv.close()
